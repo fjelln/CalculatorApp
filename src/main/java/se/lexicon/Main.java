fd966bc2ab06b5206f1ef1f4697a1db59101e44f
fjelln <nicklasfjellman+fjelln@gmail.com>
2025-03-09 19:44:02 +0100

    added while loop to give user the option to keep run the program or close it if they type "n".

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -11,42 +11,51 @@ public class Main {
         double secondNumber;
         double result = 0;
         boolean dividedByZero = false;
+        String runAgain;
 
         System.out.println("Hello and welcome!");
-        System.out.print("Write a number: ");
-        firstNumber = input.nextDouble();
         while(true) {
-            System.out.print("Choose an operator (+, -, *, /): ");
-            operator = input.next();
-            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
-                break;
-            } else {
-                System.out.println("Try again!");
-            }
-        }
-        System.out.print("Write a number: ");
-        secondNumber = input.nextDouble();
-
-        switch(operator) {
-            case "+":
-                result = firstNumber + secondNumber;
-                break;
-            case "-":
-                result = firstNumber - secondNumber;
-                break;
-            case "*":
-                result = firstNumber * secondNumber;
-                break;
-            case "/":
-                if (secondNumber == 0.0) {
-                    System.out.println("Can't divide by zero!");
-                    dividedByZero = true;
+            System.out.print("Write a number: ");
+            firstNumber = input.nextDouble();
+            while(true) {
+                System.out.print("Choose an operator (+, -, *, /): ");
+                operator = input.next();
+                if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                    break;
+                } else {
+                    System.out.println("Try again!");
                 }
-                result = firstNumber / secondNumber;
+            }
+            System.out.print("Write a number: ");
+            secondNumber = input.nextDouble();
+
+            switch(operator) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber == 0.0) {
+                        System.out.println("Can't divide by zero!");
+                        dividedByZero = true;
+                    }
+                    result = firstNumber / secondNumber;
+                    break;
+            }
+            if (!dividedByZero) {
+                System.out.println(result);
+            }
+            System.out.print("Again? (y/n): ");
+            runAgain = input.next();
+            if (runAgain.equals("n")) {
+                System.out.print("Bye!");
                 break;
-        }
-        if (!dividedByZero) {
-            System.out.println(result);
+            }
         }
     }
 }
