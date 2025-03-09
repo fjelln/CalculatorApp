4bcad1554d160c789bc122f20e47a853facf18cc
fjelln <nicklasfjellman+fjelln@gmail.com>
2025-03-09 19:17:28 +0100

    first basic calculator.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,17 +1,38 @@
 package se.lexicon;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        double firstNumber;
+        String operator;
+        double secondNumber;
+        double result = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Hello and welcome!");
+        System.out.print("Write a number: ");
+        firstNumber = input.nextDouble();
+        System.out.print("Choose an operator (+, -, *, /): ");
+        operator = input.next();
+        System.out.print("Write a number: ");
+        secondNumber = input.nextDouble();
+
+        switch(operator) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+            default:
+                System.out.println("Invalid operator: " + operator);
         }
+        System.out.println(result);
     }
 }
